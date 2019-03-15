b611b1884bde9ae29cae08730454f0b57077e1a9
木村　幸介 <kosukekimura0528@gmail.com>
2019-03-15 12:01:20 +0900

    add user new/show

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  validates :name, presence:true,length:{maximum:100 }
+  validates :email, presence: true,length: { maximum:255 },
+            format:{with:/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+            uniqueness: true
+  validates :password,presence: true,length:{minimum:6}
+
+  has_secure_password
+  before_validation { email.downcase! }
 end
